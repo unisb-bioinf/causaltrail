725de2d074d15106ef5619c810e3ede08b93aefb
Florian Schmidt <Florian.Schmidt@bioinf.uni-sb.de>
2014-12-08 15:57:25 +0100

    Comments added to Combinations.h

--- Combinations.h
+++ Combinations.h
@@ -7,27 +7,49 @@
 template<typename T>
 class Combinations{
 	public:
+	/**Combinations()
+	 *
+	 * @param keys a vector containing a key for each exchangeable position
+	 * @param values a map that holds the possible values for each key
+	 *
+	 * @return a Combinations Object
+	 *
+	 * This class creates all enumerations for the enumeration problem without replacement and without regard to sequence
+	 */
 	Combinations(const std::vector<unsigned int>& keys, const std::vector<std::vector<T>>& values);
+
+	/**createCombinations
+	 *
+	 * @param position The position of the start key (0 if all enumeration should be constructed)
+	 *
+	 * Recursively creates a enumeration of all possible combinations given the keys and their possible values
+	 */
 	void createCombinations(unsigned int position);
+
+	/**getResult
+	 *
+	 * @return vector Containing all possible combinations
+	 *
+	 * Returns the enumeration result
+	 */
 	std::vector<std::vector<T>> getResult();
+
 	private:
+
+	//Vector to store the used keys
 	const std::vector<unsigned int>& keys_;
+
+	//Vector to store the possible values per key
 	const std::vector<std::vector<T>>& values_;
+
+	//Vector to store the temporal assignment
 	std::vector<T> temp_;
+
+	//Vector to store the entire enumeration
 	std::vector<std::vector<T>> result_;
 };
 #endif
 
-
-/**Combinations()
- *
- * @param keys a vector containing a key for each exchangeable position
- * @param values a map that holds the possible values for each key
- *
- * @return a Combinations Object
- *
- * This class creates all enumerations for the enumeration problem without replacement and without regard to sequence
- */
 template<typename T>
 Combinations<T>::Combinations(const std::vector<unsigned int>& keys, const std::vector<std::vector<T>>& values)
 	:keys_(keys),values_(values)
@@ -35,15 +57,6 @@ Combinations<T>::Combinations(const std::vector<unsigned int>& keys, const std::
 	temp_.resize(values.size());
 	}
 
-/**createCombinations
- *
- * @param
- * @param
- *
- * @return void
- *
- * Recursively creates a enumeration of all possible combinations given the keys and their possible values
- */
 template<typename T>
 void Combinations<T>::createCombinations(unsigned int position=0){
 	if (position == keys_.size()){
@@ -57,13 +70,6 @@ void Combinations<T>::createCombinations(unsigned int position=0){
 		}	
 	}
 
-
-/**getResult
- *
- * @return A vector containing all possible combinations
- *
- * Returns the enumeration result
- */
 template<typename T>
 std::vector<std::vector<T>> Combinations<T>::getResult(){
 	return result_;
