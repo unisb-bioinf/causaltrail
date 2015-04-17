557e59c8c8559ac0fc7cc8ae61e03791ff3a01aa
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-04-17 14:49:11 +0200

    Adapt CMake to also work under Windows

---
+++ cmake/CompilerSpecific.cmake
@@ -0,0 +1,8 @@
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+	add_definitions(-std=c++11 -Wall -pedantic)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+	add_definitions(-std=c++11 -Wall -pedantic)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+	add_definitions(-DBOOST_ALL_NO_LIB /W4)
+endif()
+
