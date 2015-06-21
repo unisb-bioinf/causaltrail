917334ade600df8cad056f64d70b4eff42c981f6
Florian <fschmidt@bioinf.uni-sb.de>
2015-06-21 16:59:58 +0200

    Added Discretisation Tests for missing Data situations

--- test/DiscretiserTest.cpp
+++ test/DiscretiserTest.cpp
@@ -25,6 +25,19 @@ TEST_F(DiscretiserTest,Floor){
 	ASSERT_EQ(4,dObs(5,0));
 }
 
+TEST_F(DiscretiserTest,FloorIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(0,dObs(0,0));
+	ASSERT_EQ(1,dObs(1,0));
+	ASSERT_EQ(-1,dObs(2,0));
+	ASSERT_EQ(2,dObs(3,0));
+	ASSERT_EQ(3,dObs(4,0));
+	ASSERT_EQ(4,dObs(5,0));
+}
+
 TEST_F(DiscretiserTest,Ceil){
 	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
@@ -38,12 +51,38 @@ TEST_F(DiscretiserTest,Ceil){
 	ASSERT_EQ(4,dObs(5,1));
 }
 
+TEST_F(DiscretiserTest,CeilIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(0,dObs(0,1));
+	ASSERT_EQ(-1,dObs(1,1));
+	ASSERT_EQ(1,dObs(2,1));
+	ASSERT_EQ(2,dObs(3,1));
+	ASSERT_EQ(3,dObs(4,1));
+	ASSERT_EQ(4,dObs(5,1));
+}
+
 TEST_F(DiscretiserTest,Round){
 	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
 	Network n;
 	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
 	ASSERT_EQ(0,dObs(0,2));
+	ASSERT_EQ(0,dObs(1,2));
+	ASSERT_EQ(0,dObs(2,2));
+	ASSERT_EQ(1,dObs(3,2));
+	ASSERT_EQ(2,dObs(4,2));
+	ASSERT_EQ(3,dObs(5,2));
+}
+
+TEST_F(DiscretiserTest,RoundIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(-1,dObs(0,2));
 	ASSERT_EQ(0,dObs(1,2));
 	ASSERT_EQ(0,dObs(2,2));
 	ASSERT_EQ(1,dObs(3,2));
@@ -64,12 +103,38 @@ TEST_F(DiscretiserTest,AMean){
 	ASSERT_EQ(1,dObs(5,3));
 }
 
+TEST_F(DiscretiserTest,AMeanIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(0,dObs(0,3));
+	ASSERT_EQ(0,dObs(1,3));
+	ASSERT_EQ(-1,dObs(2,3));
+	ASSERT_EQ(1,dObs(3,3));
+	ASSERT_EQ(1,dObs(4,3));
+	ASSERT_EQ(1,dObs(5,3));
+}
+
 TEST_F(DiscretiserTest,HMean){
 	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
 	Network n;
 	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
 	ASSERT_EQ(0,dObs(0,4));
+	ASSERT_EQ(0,dObs(1,4));
+	ASSERT_EQ(0,dObs(2,4));
+	ASSERT_EQ(1,dObs(3,4));
+	ASSERT_EQ(1,dObs(4,4));
+	ASSERT_EQ(1,dObs(5,4));
+}
+
+TEST_F(DiscretiserTest,HMeanIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(-1,dObs(0,4));
 	ASSERT_EQ(0,dObs(1,4));
 	ASSERT_EQ(0,dObs(2,4));
 	ASSERT_EQ(1,dObs(3,4));
@@ -90,6 +155,19 @@ TEST_F(DiscretiserTest,Median){
 	ASSERT_EQ(1,dObs(5,5));
 }
 
+TEST_F(DiscretiserTest,MedianIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(0,dObs(0,5));
+	ASSERT_EQ(0,dObs(1,5));
+	ASSERT_EQ(0,dObs(2,5));
+	ASSERT_EQ(1,dObs(3,5));
+	ASSERT_EQ(1,dObs(4,5));
+	ASSERT_EQ(-1,dObs(5,5));
+}
+
 TEST_F(DiscretiserTest,Manually){
 		Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
@@ -101,6 +179,20 @@ TEST_F(DiscretiserTest,Manually){
 	ASSERT_EQ(0,dObs(2,6));
 	ASSERT_EQ(1,dObs(3,6));
 	ASSERT_EQ(1,dObs(4,6));
+	ASSERT_EQ(1,dObs(5,6));
+}
+
+TEST_F(DiscretiserTest,ManuallyIncludingNA){
+		Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	
+	ASSERT_EQ(0,dObs(0,6));
+	ASSERT_EQ(0,dObs(1,6));
+	ASSERT_EQ(0,dObs(2,6));
+	ASSERT_EQ(1,dObs(3,6));
+	ASSERT_EQ(-1,dObs(4,6));
 	ASSERT_EQ(1,dObs(5,6));
 }
 
@@ -117,6 +209,19 @@ TEST_F(DiscretiserTest,BracketMedians){
 	ASSERT_EQ(1,dObs(5,7));
 }
 
+TEST_F(DiscretiserTest,BracketMediansIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(0,dObs(0,7));
+	ASSERT_EQ(1,dObs(1,7));
+	ASSERT_EQ(0,dObs(2,7));
+	ASSERT_EQ(-1,dObs(3,7));
+	ASSERT_EQ(1,dObs(4,7));
+	ASSERT_EQ(1,dObs(5,7));
+}
+
 TEST_F(DiscretiserTest,PerasonTukey){
 	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
@@ -124,7 +229,20 @@ TEST_F(DiscretiserTest,PerasonTukey){
 	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
 	ASSERT_EQ(0,dObs(0,8));
 	ASSERT_EQ(1,dObs(1,8));
-	ASSERT_EQ(0,dObs(2,8));
+	ASSERT_EQ(1,dObs(2,8));
+	ASSERT_EQ(1,dObs(3,8));
+	ASSERT_EQ(2,dObs(4,8));
+	ASSERT_EQ(2,dObs(5,8));
+}
+
+TEST_F(DiscretiserTest,PerasonTukeyIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(-1,dObs(0,8));
+	ASSERT_EQ(1,dObs(1,8));
+	ASSERT_EQ(1,dObs(2,8));
 	ASSERT_EQ(1,dObs(3,8));
 	ASSERT_EQ(1,dObs(4,8));
 	ASSERT_EQ(2,dObs(5,8));
@@ -143,6 +261,19 @@ TEST_F(DiscretiserTest,MapNamesToInt){
 	ASSERT_EQ(0,dObs(5,9));
 }
 
+TEST_F(DiscretiserTest,MapNamesToIntNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);
+	ASSERT_EQ(1,dObs(0,9));
+	ASSERT_EQ(-1,dObs(1,9));
+	ASSERT_EQ(1,dObs(2,9));
+	ASSERT_EQ(1,dObs(3,9));
+	ASSERT_EQ(0,dObs(4,9));
+	ASSERT_EQ(0,dObs(5,9));
+}
+
 TEST_F(DiscretiserTest, Z){
 	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservations.txt"),false,true);
 	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
@@ -154,4 +285,17 @@ TEST_F(DiscretiserTest, Z){
 	ASSERT_EQ(0,dObs(3,10));
 	ASSERT_EQ(0,dObs(4,10));
 	ASSERT_EQ(0,dObs(5,10));
+}
+
+TEST_F(DiscretiserTest, ZIncludingNA){
+	Matrix<std::string> oriObs (TEST_DATA_PATH("testObservationsIncludingNA.txt"),false,true);
+	Matrix<int> dObs (oriObs.getColCount(), oriObs.getRowCount(),0);
+	Network n;
+	Discretiser d (oriObs,TEST_DATA_PATH("jsonDiscretiserTest.json"),dObs,n);	
+	ASSERT_EQ(0,dObs(0,10));
+	ASSERT_EQ(0,dObs(1,10));
+	ASSERT_EQ(0,dObs(2,10));
+	ASSERT_EQ(0,dObs(3,10));
+	ASSERT_EQ(0,dObs(4,10));
+	ASSERT_EQ(-1,dObs(5,10));
 }
