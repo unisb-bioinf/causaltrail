cb351e64712e13325aeb6f0322bac4b80e06d9ce
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2015-11-27 15:42:45 +0100

    Reformat core/main.cpp
    This makes the code much more understandable.

--- core/main.cpp
+++ core/main.cpp
@@ -2,10 +2,10 @@
 #include "Parser.h"
 #include <iostream>
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
 	NetworkController c;
-	if (argc < 4) {
+	if(argc < 4) {
 		std::cout
 		    << "Insufficient number of parameters\n\n"
 		    << "Usage:\n\t" << argv[0]
@@ -13,41 +13,50 @@ int main(int argc, char *argv[]){
 		    << "or:\n\t" << argv[0]
 		    << " observations.txt discretisation_control.json network.sif "
 		       "network.na\n";
+
+		return -1;
 	}
-	else{
-		std::string datafile=argv[1];
-		std::string controlfile=argv[2];
-		std::string networkfile=argv[3];
-		if (argc == 5){
-			std::string nodefile=argv[4];
-			c.loadNetwork(nodefile);
-		}
-		c.loadNetwork(networkfile);
-		c.loadObservations(datafile,controlfile);
-		c.trainNetwork();
-		std::cout<<c.getNetwork()<<std::endl;
-		std::cout<<"Number of EM runs: "<<c.getNumberOfEMRuns()<<std::endl;
-		std::cout<<"Time used for training: "<<c.getTimeInMicroSeconds()<<"µs"<<std::endl;
-		std::string input = "";
-		std::cout<<"Please enter a query"<<std::endl;
-		std::getline(std::cin,input);
-		while (input != "exit"){
-			try{
-				Parser p3 = Parser(input,c);
-				QueryExecuter qe3 = p3.parseQuery();
-				std::pair<float, std::vector<std::string>> respair = qe3.execute();
-				std::cout<<respair.first<<std::endl;
-				for (const auto& arg: respair.second){
-					std::cout<<arg<<"\n";
-					}
-				std::cout<<std::endl;
-			}
-			catch (std::exception& e){
-				std::cerr<<e.what()<<std::endl;
-			}
-			std::cout<<"Please enter a query"<<std::endl;
-			std::getline(std::cin,input);
-		}
+
+	std::string datafile = argv[1];
+	std::string controlfile = argv[2];
+	std::string networkfile = argv[3];
+
+	if(argc == 5) {
+		std::string nodefile = argv[4];
+		c.loadNetwork(nodefile);
 	}
+
+	c.loadNetwork(networkfile);
+	c.loadObservations(datafile, controlfile);
+	c.trainNetwork();
+
+	std::cout << c.getNetwork()
+	          << "\nNumber of EM runs: " << c.getNumberOfEMRuns()
+	          << "\nTime used for training: " << c.getTimeInMicroSeconds()
+	          << "µs" << std::endl;
+
+	std::string input = "";
+	std::cout << "Please enter a query" << std::endl;
+
+	std::getline(std::cin, input);
+	while(input != "exit") {
+		try {
+			Parser p3 = Parser(input, c);
+			QueryExecuter qe3 = p3.parseQuery();
+
+			auto result = qe3.execute();
+			std::cout << result.first << std::endl;
+			for(const auto& arg : result.second) {
+				std::cout << arg << "\n";
+			}
+			std::cout << std::endl;
+		} catch(std::exception& e) {
+			std::cerr << e.what() << std::endl;
+		}
+
+		std::cout << "Please enter a query" << std::endl;
+		std::getline(std::cin, input);
+	}
+
 	return 0;
 }
